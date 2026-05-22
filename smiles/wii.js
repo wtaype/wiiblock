35ddff499306831d3a7a22a05ee941fb258a0ba0
wtaype <wilder.taype16@gmail.com>
2026-05-21 22:21:03 -0500

    Actualizacion Principal v10.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -10,22 +10,19 @@ export let lanzamiento = 2026;
 export let by = '@wilder.taype';
 export let linkme = 'https://wtaype.github.io/';
 export let ipdev = import.meta.env.VITE_DEV;
-export let version = '10';
+export let version = 'v10';
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag 10 -m "Version 10" ; git push origin 10
+git tag v10 -m "Version v10" ; git push origin v10
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal 10.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v10.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
-git tag -d 10 ; git tag 10 -m "Version 10 actualizada" ; git push origin 10 --force
+git tag -d v10 ; git tag v10 -m "Version v10 actualizada" ; git push origin v10 --force
 
-// PARA ACTUALIZAR SITEMAP EFFICIENTE (4)
-npm run sitemap
-
-// Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (5)
+// Actualizar versiones de seguridad [ELIMINAR CARPETA - ARCHIVO ONLINE] (4)
 git rm --cached skills-lock.json ; git commit -m "Archivo Eliminado" ; git push origin main
-git rm -r --cached .claude/ ; git commit -m "Carpeta Eliminada" ; git push origin main
-
+git rm -r --cached .claude/ ; git commit -m "Carpeta Eliminada" ; git push origin main 
+git tag -d 10 ; git push origin --delete 10 // Eliminar tag del local y remoto.
  ACTUALIZACION TAG[END] */
